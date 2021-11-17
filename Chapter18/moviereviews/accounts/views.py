0db00f7c5e8870f2eb5369462609e1c564a45702
Daniel Correa <yo@danielgara.com>
2021-11-17 10:16:53 -0500

    imp: Chapter18

---
+++ Chapter18/moviereviews/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def signupaccount(request):
+    return render(request, 'signupaccount.html', 
+                  {'form':UserCreationForm})
+
